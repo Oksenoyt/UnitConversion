e9959b289031c8ef6080cdc01dc409d21bd7cdda
Helen Oksenoyt <h.oksenoyt@gmail.com>
2024-07-03 16:57:30 -1000

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Elena Kholodilina on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
